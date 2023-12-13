5113db73f5b8d5038e96f8e0a4beb466366134d7
Alen Antony <alenantony32@gmail.com>
2023-12-13 23:00:01 +0530

    billno

--- zoho_django/settings.py
+++ zoho_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s(rbc&ex(i8#zax7zkr!$v_*sxigmhheb4s_-u=m=&pow58fl2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -131,10 +131,14 @@ APPEND_SLASH = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
